290001f4fe04786c2b82e53bc9cd8258286aa123
Argyraspides <joshivinayak441@gmail.com>
2025-10-04 19:35:12 +1300

    Added utilities from hermes/gaia repositories

---
+++ Enums/ImageType.cs
@@ -0,0 +1,11 @@
+namespace Daedalus.Enums;
+
+public enum ImageType
+{
+  Bmp = 0,
+  Jpeg = 1,
+  Gif = 2,
+  Tiff = 3,
+  Png = 4,
+  Unknown = int.MaxValue
+}
---
+++ Networking/Cache/ICacheCapability.cs
@@ -0,0 +1,38 @@
+/*
+
+
+
+
+88        88  88888888888  88888888ba   88b           d88  88888888888  ad88888ba
+88        88  88           88      "8b  888b         d888  88          d8"     "8b
+88        88  88           88      ,8P  88`8b       d8'88  88          Y8,
+88aaaaaaaa88  88aaaaa      88aaaaaa8P'  88 `8b     d8' 88  88aaaaa     `Y8aaaaa,
+88""""""""88  88"""""      88""""88'    88  `8b   d8'  88  88"""""       `"""""8b,
+88        88  88           88    `8b    88   `8b d8'   88  88                  `8b
+88        88  88           88     `8b   88    `888'    88  88          Y8a     a8P
+88        88  88888888888  88      `8b  88     `8'     88  88888888888  "Y88888P"
+
+
+                            MESSENGER OF THE MACHINES
+
+*/
+
+namespace Daedalus.Networking.Cache;
+
+public interface ICacheCapability<DaedalusResource>
+{
+    void CacheResource(DaedalusResource resource);
+    DaedalusResource RetrieveResourceFromCache(string resourceHash);
+
+    /// <summary>
+    /// Can be used to retrieve a resource from cache if the provided resource argument contains enough
+    /// information in order to determine the identity of the full resource with all its fields.
+    /// </summary>
+    /// <param name="partialResource"> The resource with parts of its fields filled out which contains the necessary information
+    /// to uniquely identify the entire resource</param>
+    /// <returns></returns>
+    DaedalusResource RetrieveResourceFromCache(DaedalusResource partialResource);
+
+    bool ResourceExists(string resourceHash);
+    bool ResourceExists(DaedalusResource partialResource);
+}
